65e95b374de22f66c520496fdfdf744a26d92844
Thomas Nabord <thomas.nabord@prestashop.com>
2015-05-22 16:13:37 +0200

    New generic URL to download PrestaShop

--- scripts/get_prestashop.sh
+++ scripts/get_prestashop.sh
@@ -2,6 +2,6 @@
 
 logger "Downloading PrestaShop ..."
 
-wget "https://www.prestashop.com/download/old/prestashop_1.6.0.14.zip" -O prestashop.zip
+wget "https://www.prestashop.com/download/latest " -O prestashop.zip
 
 logger "PrestaShop downloaded"
