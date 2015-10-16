1101a038f632e7c6457c926385e14a714adfea38
Thomas Nabord <thomas.nabord@prestashop.com>
2015-10-16 14:32:26 +0200

    Add prestashop download at first boot

---
+++ conf_files/prestashop_first_boot.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+which apache2 > /dev/null
+ if [ $? != 0 ]; then
+ 	root_path="/usr/share/nginx/www/";
+ else
+ 	root_path="/var/www/html/";
+ fi
+
+FLAG="/var/log/prestashop_init.log"
+if [ ! -f $FLAG ]; then
+
+    logger "Downloading PrestaShop ..."
+
+    wget "https://www.prestashop.com/download/latest" -O /tmp/prestashop.zip
+     if [ $? = 0 ]; then
+
+        logger "PrestaShop downloaded sucessfully"
+        rm -rf $root_path*
+
+        logger "Preparing PrestaShop ..."
+
+        unzip -q /tmp/prestashop.zip
+        mv /tmp/prestashop/* $root_path
+
+        rmdir /tmp/prestashop
+        rm /tmp/prestashop.zip
+        cd $root_path
+        rm index.html
+
+        chown -R www-data:www-data $root_path;
+        chmod -R g+w $root_path;
+
+        logger "PrestaShop is ready to be installed !"
+    fi
+
+    touch $FLAG
+fi
---
+++ scripts/add_boot_script.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+logger "Add script at boot"
+
+mv /tmp/prestashop_first_boot.sh /usr/local/bin/prestashop_first_boot.sh
+sed -i -e 's#exit 0#/usr/local/bin/prestashop_first_boot.sh\nexit 0#' /etc/rc.local
+
+logger "Script added"
